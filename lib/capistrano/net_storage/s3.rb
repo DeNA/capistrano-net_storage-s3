66eaf3e58464572c2b9c6c731e723398e401cbcb
progrhyme <progrhyme@gmail.com>
2017-04-12 17:41:07 +0900

    Initialize by "bundle gem capistrano-net_storage-s3"

---
+++ lib/capistrano/net_storage/s3.rb
@@ -0,0 +1,9 @@
+require "capistrano/net_storage/s3/version"
+
+module Capistrano
+  module NetStorage
+    module S3
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano/net_storage/s3/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module NetStorage
+    module S3
+      VERSION = "0.1.0"
+    end
+  end
+end
