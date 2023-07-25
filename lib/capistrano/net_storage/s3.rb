4645db3b38c73ba348279892ff2d9854c10f2ad5
Takumasa Ochi <aeroastro007@gmail.com>
2023-07-25 12:16:20 +0900

    Move Capistrano::NetStorage::S3 to capistrano/net_storage/s3.rb

--- lib/capistrano/net_storage/s3.rb
+++ lib/capistrano/net_storage/s3.rb
@@ -1,8 +1,18 @@
-require 'capistrano/net_storage/s3/base'
-require 'capistrano/net_storage/s3/error'
 require 'capistrano/net_storage/s3/config'
-require 'capistrano/net_storage/s3/broker/aws_cli'
 require 'capistrano/net_storage/s3/transport'
-require 'capistrano/net_storage/s3/version'
+
+module Capistrano
+  module NetStorage
+    class S3
+      class << self
+        attr_reader :config
+
+        def setup!(config:)
+          @config = config
+        end
+      end
+    end
+  end
+end
 
 Capistrano::NetStorage::S3.setup!(config: Capistrano::NetStorage::S3::Config.new)
