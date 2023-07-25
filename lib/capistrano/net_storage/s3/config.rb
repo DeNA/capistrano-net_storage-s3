53f219638714f3347f68ba44b455afaa7133321f
Takumasa Ochi <aeroastro007@gmail.com>
2023-07-25 12:23:37 +0900

    Rewrite and Improve Capistrano::NetStorage::S3::Config
    * Remove caching which has little effect on perfomance
    * Simplify compacting logic on AWS environments
    * Minimize document to increase maintainability
    * Make methods as private as possible to encapsulate unnecessary information from users
    * Make archives_url and URL-related methods more robust against settings and usage

--- lib/capistrano/net_storage/s3/config.rb
+++ lib/capistrano/net_storage/s3/config.rb
@@ -1,132 +1,82 @@
 require 'uri'
 
-require 'capistrano/net_storage/s3/base'
-require 'capistrano/net_storage/s3/error'
 require 'capistrano/net_storage/s3/broker/aws_cli'
 
 class Capistrano::NetStorage::S3
   class Config
-    # Broker object for transport operations
-    # @return [Capistrano::NetStorage::S3::Broker::Base]
-    def broker
-      @broker ||= begin
-        case fetch(:net_storage_s3_broker, :aws_cli)
-        when :aws_cli
-          Broker::AwsCLI.new
-        else
-          raise Capistrano::NetStorage::S3::Error, "No broker defined! #{fetch(:net_storage_s3_broker)}"
-        end
+    def broker_class
+      case fetch(:net_storage_s3_broker, :aws_cli)
+      when :aws_cli
+        Broker::AwsCLI
+      else
+        raise ArgumentError, "No broker defined! #{fetch(:net_storage_s3_broker)}"
       end
     end
 
-    # AWS configurations
-
-    # @return [String] AWS Access Key ID
-    def aws_access_key_id
-      @aws_access_key_id ||= fetch(:net_storage_s3_aws_access_key_id, ENV['AWS_ACCESS_KEY_ID'])
-    end
-
-    # @return [String] AWS Secret Access Key
-    def aws_secret_access_key
-      @aws_secret_access_key ||= fetch(:net_storage_s3_aws_secret_access_key)
-      @aws_secret_access_key ||= ENV['AWS_SECRET_ACCESS_KEY']
-    end
-
-    # @return [String] AWS Session Token
-    def aws_session_token
-      @aws_session_token ||= fetch(:net_storage_s3_aws_session_token, ENV['AWS_SESSION_TOKEN'])
-    end
-
-    # @return [String] AWS Region
-    def aws_region
-      @aws_region ||= fetch(:net_storage_s3_aws_region, ENV['AWS_DEFAULT_REGION'])
-    end
-
-    # @return [String] AWS Profile
-    def aws_profile
-      @aws_profile ||= fetch(:net_storage_s3_aws_profile, ENV['AWS_DEFAULT_PROFILE'])
-    end
-
-    # AWS Config File
-    def aws_config_file
-      @aws_config_file ||= fetch(:net_storage_s3_aws_config_file, ENV['AWS_CONFIG_FILE'])
-    end
-
-    # @return [Hash] AWS environment variables
     def aws_environments
-      @aws_environments ||= begin
-        environments = {}
-        environments[:aws_config_file]       = aws_config_file       if aws_config_file
-        environments[:aws_default_profile]   = aws_profile           if aws_profile
-        environments[:aws_default_region]    = aws_region            if aws_region
-        environments[:aws_access_key_id]     = aws_access_key_id     if aws_access_key_id
-        environments[:aws_secret_access_key] = aws_secret_access_key if aws_secret_access_key
-        environments[:aws_session_token]     = aws_session_token     if aws_session_token
-        environments
-      end
+      {
+        aws_config_file: aws_config_file,
+        aws_default_profile: aws_profile,
+        aws_default_region: aws_region,
+        aws_access_key_id: aws_access_key_id,
+        aws_secret_access_key: aws_secret_access_key,
+        aws_session_token: aws_session_token,
+      }.compact
     end
 
-    # S3 bucket name via which one transports application archives
-    # @return [String]
     def bucket
-      @bucket ||= begin
-        unless bucket = fetch(:net_storage_s3_bucket)
-          raise Capistrano::NetStorage::S3::Error, ':net_storage_s3_bucket is not configured!'
-        end
-        bucket
-      end
+      fetch(:net_storage_s3_bucket, -> { raise ArgumentError, ':net_storage_s3_bucket is not configured!' })
     end
 
-    # S3 bucket URL via which one transports application archives
-    # @return [URI::Generic]
-    def bucket_url
-      @bucket_url ||= URI.parse("s3://#{bucket}")
-    end
-
-    # Directory path on S3 bucket for application archives
-    # @return [String]
-    def archives_directory
-      # append '/' in case missing
-      @archives_directory ||= begin
-        dir = fetch(:net_storage_s3_archives_directory)
-        dir.sub(%r{[^/]\Z}, '\&/') if dir
-      end
-    end
-
-    # S3 URL which contains application archives
-    # @return [URI::Generic]
     def archives_url
-      @archives_url ||= begin
-        if archives_directory
-          bucket_url + archives_directory
-        else
-          bucket_url
-        end
-      end
+      URI.parse("s3://#{bucket}") + archives_directory
     end
 
-    # S3 URL of the application archive for current deployment
-    # @return [URI::Generic]
     def archive_url
-      @archive_url ||= begin
-        unless revision = fetch(:current_revision)
-          raise ArgumentError, ':current_revision is not set!'
-        end
-        archive_file = "#{revision}.#{Capistrano::NetStorage.config.archive_file_extension}"
-        archives_url + archive_file
-      end
+      revision = fetch(:current_revision)
+      raise ArgumentError, ':current_revision is not set! Your deployment flow might be buggy.' unless revision
+
+      archives_url + "#{revision}.#{Capistrano::NetStorage.config.archive_file_extension}"
     end
 
-    # Max retrial number for S3 operations
-    # @return [Fixnum]
-    def max_retry
-      @max_retry ||= fetch(:net_storage_s3_max_retry, 3)
-    end
-
-    # Number to keep archives on S3
-    # @return [Fixnum]
     def s3_keep_releases
-      @s3_keep_releases ||= fetch(:net_storage_s3_keep_releases, fetch(:keep_releases, 5))
+      fetch(:net_storage_s3_keep_releases, fetch(:keep_releases))
+    end
+
+    private
+
+    def archives_directory
+      directory = fetch(:net_storage_s3_archives_directory, '/')
+
+      # Make it absolute directory path from root
+      directory = "#{directory}/" unless directory.end_with?('/')
+      directory = "/#{directory}" unless directory.start_with?('/')
+
+      directory
+    end
+
+    def aws_access_key_id
+      fetch(:net_storage_s3_aws_access_key_id, ENV['AWS_ACCESS_KEY_ID'])
+    end
+
+    def aws_secret_access_key
+      fetch(:net_storage_s3_aws_secret_access_key, ENV['AWS_SECRET_ACCESS_KEY'])
+    end
+
+    def aws_session_token
+      fetch(:net_storage_s3_aws_session_token, ENV['AWS_SESSION_TOKEN'])
+    end
+
+    def aws_region
+      fetch(:net_storage_s3_aws_region, ENV['AWS_DEFAULT_REGION'])
+    end
+
+    def aws_profile
+      fetch(:net_storage_s3_aws_profile, ENV['AWS_DEFAULT_PROFILE'])
+    end
+
+    def aws_config_file
+      fetch(:net_storage_s3_aws_config_file, ENV['AWS_CONFIG_FILE'])
     end
   end
 end
