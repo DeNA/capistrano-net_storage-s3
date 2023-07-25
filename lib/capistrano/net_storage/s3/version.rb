80f6a416a74535dd9708026d9fe9934219357f6c
Takumasa Ochi <aeroastro007@gmail.com>
2023-07-25 12:17:30 +0900

    Fix module and class definition for Capistrano::NetStorage::S3::VERSION

--- lib/capistrano/net_storage/s3/version.rb
+++ lib/capistrano/net_storage/s3/version.rb
@@ -1,5 +1,7 @@
-require 'capistrano/net_storage/s3/base'
-
-class Capistrano::NetStorage::S3
-  VERSION = '0.3.0'
+module Capistrano
+  module NetStorage
+    class S3
+      VERSION = '0.3.0'
+    end
+  end
 end
